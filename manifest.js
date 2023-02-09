ba1da5d3c46f5b82607168b2ed08bd1888614f56
Perez <manuel.perez@basworld.com>
2023-02-09 21:17:46 +0100

    Added script to update manifest

--- manifest.js
+++ manifest.js
@@ -1,19 +1,35 @@
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
+
+const ERP2_API_URL = 'http://localhost:8005';
+const ERP2_API_VERSION = 'v1';
+
+const client = axios.create({
+    baseURL: `${ERP2_API_URL}/api/${ERP2_API_VERSION}`,
+    timeout: 20000,
+    withCredentials: false,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
 
 const resourcesFolder = './dist';
-const baseURL = 'https://cdn.jsdelivr.net/gh/perezmaz/test1/dist';
+
 const {
     name,
     description = '',
     version = '0'
 } = require('./package.json');
 
+const baseURL = `https://cdn.jsdelivr.net/gh/perezmaz/test1@${version}/dist`;
+
 const manifest = {
     name,
     description,
     version,
     type: 'JS_SINGLE_PAGE',
+    manifest: `${baseURL}/manifest.json`,
     resources: {
         js: [],
         css: [],
@@ -46,6 +62,30 @@ const readFolder = (folderPath, nestedFolder) => {
     });
 }
 
+const sendManifest = async (manifest) => {
+    const request = {
+        data: manifest,
+    };
+
+    try {
+        const response = await client.post('/applications', request);
+        return {
+            status: response.status,
+            data: response.data,
+        };
+    } catch (error) {
+        return {
+            status: error.response.status,
+            error: error.response.data,
+        };
+    }
+}
+
 readFolder(resourcesFolder, '');
 
 fs.writeFileSync(`${resourcesFolder}/manifest.json`, JSON.stringify(manifest));
+
+sendManifest(manifest).then(({ data = 'No data', error = 'No errors', status }) => {
+    console.log(status, data, error);
+});
+
