957b18d9c26368e8e15d3c1566b21afe67d4e192
Perez <manuel.perez@basworld.com>
2023-02-10 09:48:18 +0100

    (Changed) New content in main page

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -17,6 +17,14 @@
             tincidunt lobortis. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Lorem
             donec massa sapien faucibus et. Eget arcu dictum varius duis at.
         </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Id cursus metus aliquam eleifend mi in. Nunc lobortis mattis aliquam faucibus purus in.
+            Viverra aliquet eget sit amet. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Posuere
+            lorem ipsum dolor sit amet consectetur adipiscing. Est pellentesque elit ullamcorper dignissim cras
+            tincidunt lobortis. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Lorem
+            donec massa sapien faucibus et. Eget arcu dictum varius duis at.
+        </p>
         <cv-button kind="primary" class="bds-self-align-center" @click="goTo">
             Continue
         </cv-button>
